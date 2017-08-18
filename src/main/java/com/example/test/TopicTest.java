fe05a93fe98a134477f2c26c19e492c56c0d7f7d
uptowncat <158668658@qq.com>
2017-08-18 14:56:06 +0800

    消息的有状态的推送和订阅

--- src/main/java/com/example/test/TopicTest.java
+++ src/main/java/com/example/test/TopicTest.java
@@ -1,9 +1,11 @@
 package com.example.test;
 
 import com.example.util.CommonUtil;
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.junit.Test;
 
 import javax.jms.*;
+import javax.xml.soap.Text;
 
 /**
  * 主题测试
@@ -33,7 +35,7 @@ public class TopicTest {
      * @throws JMSException
      */
     @Test
-    public void testSubcribe() throws JMSException {
+    public void testSubscribe() throws JMSException {
         Connection connection = CommonUtil.getConnection();
         connection.start();
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
@@ -43,5 +45,34 @@ public class TopicTest {
         System.out.println(((TextMessage)message).getText());
         session.close();
         connection.close();
+    }
+
+    @Test
+    public void testDurablePublish() throws JMSException {
+        TopicConnectionFactory factory = new ActiveMQConnectionFactory(ActiveMQConnectionFactory.DEFAULT_BROKER_URL);
+        TopicConnection topicConnection = factory.createTopicConnection();
+        topicConnection.start();
+        TopicSession topicSession = topicConnection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
+        Topic topic = topicSession.createTopic("durabe_topic");
+        TopicPublisher publisher = topicSession.createPublisher(topic);
+        TextMessage textMessage = topicSession.createTextMessage();
+        textMessage.setText("hello, activemq");
+//        publisher.setDeliveryMode(DeliveryMode.PERSISTENT);
+        publisher.send(textMessage);
+        topicConnection.close();
+    }
+
+    @Test
+    public void testDurableSubscribe() throws JMSException {
+        TopicConnectionFactory factory = new ActiveMQConnectionFactory(ActiveMQConnectionFactory.DEFAULT_BROKER_URL);
+        TopicConnection topicConnection = factory.createTopicConnection();
+        topicConnection.setClientID("h34");
+        topicConnection.start();
+        TopicSession topicSession = topicConnection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
+        Topic topic = topicSession.createTopic("durabe_topic");
+        TopicSubscriber durableSubscriber = topicSession.createDurableSubscriber(topic, "11");
+        Message message = durableSubscriber.receive();
+        System.out.println(((TextMessage)message).getText());
+        topicConnection.close();
     }
 }
